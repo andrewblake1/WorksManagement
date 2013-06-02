ca36bb12627493bd6736ecb3d6ca6babe14dfcff
Andrew Blake <admin@newzealandfishing.com>
2013-06-02 21:50:09 +1200

    renumberdrawings utility command bug

--- protected/commands/shell/RenumberdrawingsCommand.php
+++ protected/commands/shell/RenumberdrawingsCommand.php
@@ -43,12 +43,17 @@ EOD;
 	    //check the input from the user and continue if they indicated yes to the above question
 	    if(!strncasecmp(trim(fgets(STDIN)),'y',1)) 
 		{
+			// 1st phase ensuring unique by adding max(id)
 			try
 			{
+				$max = Yii::app()->db
+					->createCommand("SELECT MAX(id) + MAX(id_new) AS max FROM tbl_drawing")
+					->queryScalar();
+				
 				Yii::app()->db->createCommand("
 					START TRANSACTION;
 					UPDATE `tbl_drawing` SET id_old = id;
-					UPDATE `tbl_drawing` SET id = id_new;
+					UPDATE `tbl_drawing` SET id = id_new + $max;
 					COMMIT;
 				")->execute();
 			}
@@ -65,6 +70,27 @@ EOD;
 				exec("mv {$path}drawing/{$drawing->id_old} {$path}drawing/{$drawing->id_new}");
 			}
 			
+			// send phase - to what we want remove max(id)
+			try
+			{
+				Yii::app()->db->createCommand("
+					START TRANSACTION;
+					UPDATE `tbl_drawing` SET id = id - $max;
+					COMMIT;
+				")->execute();
+			}
+			catch (CDbException $e)
+			{
+				echo $e->getMessage();
+				return;
+			}
+
+			// rename the directories
+			$path = Yii::app()->params['privateUploadPath'];
+			foreach(Drawing::model()->findAll() as $drawing)
+			{
+				exec("mv {$path}drawing/{$drawing->id_old} {$path}drawing/{$drawing->id_new}");
+			}
 
 			//provide a message indicating success
 			echo "Complete. Please check all ok and if not restore the backup you hopeufully made at Openhost!";
