1db06edf0cecdaa520c4d592164baac4e82bc92c
Andrew Blake <admin@newzealandfishing.com>
2013-06-13 17:05:00 +1200

    bug resolution

--- protected/models/Duty.php
+++ protected/models/Duty.php
@@ -257,7 +257,7 @@ class Duty extends ActiveRecord
 		 */
 		$criteria->join="
 			JOIN tbl_duty_step_dependency dutyStepDependency ON t.duty_step_id = dutyStepDependency.parent_duty_step_id
-			JOIN v_duty dutyChild ON dutyStepDependency.child_duty_step_id = dutyChild.id 
+			JOIN v_duty dutyChild ON dutyStepDependency.child_duty_step_id = dutyChild.duty_step_id 
 		";
 		
 		// this duties id
